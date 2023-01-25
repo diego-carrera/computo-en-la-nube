cf65bf43846bb86cffc4756452e3868de1f376e7
Diego Carrera Nicholls <diego.carrera@gmail.com>
2023-01-24 21:59:46 -0600

    mejoras menores y valores por defecto

--- programacion-solucion-paralela/suma_arreglos.cpp
+++ programacion-solucion-paralela/suma_arreglos.cpp
@@ -24,12 +24,34 @@ int main()
   cout << "Nota: Utiliza usleep para demostrar la utilidad de los hilos" << endl;
 
   cout << "Elige tu semilla para generar los numeros aleatorios: ";
-  cin >> SEED;
+  std::string seed_input;
+  std::getline(std::cin, seed_input);
+  if (!seed_input.empty())
+  {
+    SEED = std::stoi(seed_input);
+  }
+
+  cout << "Elige el tamaño del arreglo: ";
+  std::string n_input;
+  std::getline(std::cin, n_input);
+  if (!n_input.empty())
+  {
+    N = std::stoi(n_input);
+  }
+  if (N > 257400)
+  {
+    cout << "El tamaño del arreglo es muy grande, se usara el valor por defecto: 257400" << endl;
+    N = 257400;
+  }
 
   int cores = omp_get_num_procs();
   cout << "Elige la cantidad de hilos que quieres usar (" << cores << ") cores: ";
-  cin >> USE_THREADS;
-
+  std::string threads_input;
+  std::getline(std::cin, threads_input);
+  if (!threads_input.empty())
+  {
+    USE_THREADS = std::stoi(threads_input);
+  }
 
   std::mt19937 engine(SEED);
   std::uniform_real_distribution<double> dist(LO, HI);
@@ -37,49 +59,67 @@ int main()
   float a[N], b[N], c[N];
   int i;
 
-  for ( i=0; i<N; i++) {
+  for (i = 0; i < N; i++)
+  {
     a[i] = dist(engine);
     b[i] = dist(engine);
-
   }
 
-  if ( USE_THREADS > 0 ) {
+  if (USE_THREADS > 0)
+  {
     omp_set_num_threads(USE_THREADS);
   }
-
 
   int pedazos = chunk;
   auto start = std::chrono::high_resolution_clock::now();
 
   #pragma omp parallel for \
-  shared ( a, b, c, pedazos ) private (i) \
-  schedule (static, pedazos)
-
-  for ( i = 0; i < N; i++ ) {
+  shared(a, b, c, pedazos) private(i) \
+  schedule(static, pedazos)
+  for (i = 0; i < N; i++)
+  {
     c[i] = a[i] + b[i];
-    if ( i % 100 == 0 ) {
+    if (i % 100 == 0)
+    {
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
   }
+
+  double suma = 0.0;
+
+  for (i = 0; i < N; i++)
+  {
+    suma += c[i];
+  }
+
   auto end = std::chrono::high_resolution_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-  cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << endl;
+  cout << endl;
+  cout << "Imprimiendo los primeros " << mostrar << " valores de " << (sizeof(a) / sizeof(*a)) << " del arreglo a:" << endl;
   imprimeArreglo(a);
+  cout << endl;
 
-  cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << endl;
+  cout << "Imprimiendo los primeros " << mostrar << " valores de " << (sizeof(b) / sizeof(*b)) << " del arreglo b:" << endl;
   imprimeArreglo(b);
+  cout << endl;
 
-  cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << endl;
+  cout << "Imprimiendo los primeros " << mostrar << " valores de " << (sizeof(c) / sizeof(*c)) << " del arreglo c:" << endl;
   imprimeArreglo(c);
+  cout << endl;
 
+  cout << "Suma de los valores del arreglo c: " << suma << endl;
+
+  cout << endl;
   cout << "Tiempo de ejecucion: " << time.count() << " ms" << endl;
 
   return 0;
 }
 
-void imprimeArreglo(float *d) {
-  for (int i = 0; i < mostrar; i++) {
+void imprimeArreglo(float *d)
+{
+  for (int i = 0; i < mostrar; i++)
+  {
     cout << d[i] << " ";
   }
   cout << endl;
